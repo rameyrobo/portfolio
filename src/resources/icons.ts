f124e5a723bd8ae4d88eb85e81a325b40e2d0f0b
Ramey Robles <rameyrobo@gmail.com>
2025-09-01 08:18:38 -0700

    disabled blog and gallery

--- src/resources/icons.ts
+++ src/resources/icons.ts
@@ -25,11 +25,30 @@ import {
 import {
   SiJavascript,
   SiNextdotjs,
+  SiElementor,
   SiFigma,
+  SiGraphql,
+  SiMagento,
+  SiMysql,
+  SiMariadb,
+  SiPhp,
+  SiReact,
+  SiShopify,
   SiSupabase,
+  SiTypescript,
+  SiWoo,
 } from "react-icons/si";
 
-import { FaDiscord, FaGithub, FaLinkedin, FaX, FaThreads } from "react-icons/fa6";
+
+import { 
+  FaDiscord, 
+  FaGithub, 
+  FaLinkedin, 
+  FaX, 
+  FaThreads,
+  FaNodeJs,
+  FaWordpress,
+ } from "react-icons/fa6";
 
 export const iconLibrary: Record<string, IconType> = {
   arrowUpRight: HiArrowUpRight,
@@ -46,6 +65,8 @@ export const iconLibrary: Record<string, IconType> = {
   discord: FaDiscord,
   eye: HiOutlineEye,
   eyeOff: HiOutlineEyeSlash,
+  supabase: SiSupabase,
+  figma: SiFigma,
   github: FaGithub,
   linkedin: FaLinkedin,
   x: FaX,
@@ -54,9 +75,19 @@ export const iconLibrary: Record<string, IconType> = {
   document: HiOutlineDocument,
   rocket: HiOutlineRocketLaunch,
   javascript: SiJavascript,
+  typescript: SiTypescript,
+  react: SiReact,
   nextjs: SiNextdotjs,
-  supabase: SiSupabase,
-  figma: SiFigma,
+  php: SiPhp,
+  mysql: SiMysql,
+  mariadb: SiMariadb,
+  graphql: SiGraphql,
+  wordpress: FaWordpress,
+  elementor: SiElementor,
+  nodejs: FaNodeJs,
+  magento: SiMagento,
+  shopify: SiShopify,
+  woo: SiWoo,
 };
 
 export type IconLibrary = typeof iconLibrary;
--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -19,8 +19,8 @@ const routes: RoutesConfig = {
   "/": true,
   "/about": true,
   "/work": true,
-  "/blog": true,
-  "/gallery": true,
+  "/blog": false,
+  "/gallery": false,
 };
 
 const display: DisplayConfig = {
